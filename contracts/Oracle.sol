e73d1ba4cdc5be0a15ab1cfb20de0d3f6b078887
Jordan Holland <jordan.holland@rea-group.com>
2020-03-09 21:33:39 +1100

    Add staking activation method

--- contracts/Oracle.sol
+++ contracts/Oracle.sol
@@ -1,16 +1,47 @@
 pragma solidity >=0.4.21 <0.7.0;
 
+import { SafeMath } from "./vendors/SafeMath.sol";
+
 contract Oracle {
+  uint256 public totalStakeIntentionSupply;       // The current total ETH in waiting
+
   uint256 public totalMintedSupply;               // The current total minted supply
   uint256 public requiredStakeAmount;             // Amount of ETH required to begin staking
   uint256 public requestAmount;                   // Amount in ETH required for a request
 
-  mapping(address => bool) public pendingStakers; // Stakers who have paid ETH but not begun
+  enum StakerType {
+    INACTIVE,
+    PENDING,
+    ACTIVE
+  }
+
+  mapping(address => StakerType) public stakers;  // Current stakers
+  mapping(address => uint256) public balanceOf;   // Balance of the staker
 
   constructor(uint256 _requiredStakeAmount, uint256 _requestAmount) public {
+    totalStakeIntentionSupply = 0;
     totalMintedSupply = 0;
     requiredStakeAmount = _requiredStakeAmount;
     requestAmount = _requestAmount;
+  }
+
+  function activateStake() public {
+    require(stakers[msg.sender] == StakerType.PENDING, "Must intially be a pending staker to activate...");
+
+    if(totalMintedSupply > 0) {
+      uint256 currentBalance = address(this).balance - totalStakeIntentionSupply;
+      uint256 mintAmount = SafeMath.mul(requiredStakeAmount, totalMintedSupply)/currentBalance;
+
+      totalMintedSupply = SafeMath.add(totalMintedSupply, mintAmount);
+      balanceOf[msg.sender] = SafeMath.add(balanceOf[msg.sender], mintAmount);
+    } else {
+      uint256 initialMinted = requiredStakeAmount;
+      totalMintedSupply = initialMinted;
+      balanceOf[msg.sender] = initialMinted;
+    }
+
+    stakers[msg.sender] = StakerType.ACTIVE;
+    totalStakeIntentionSupply -= requiredStakeAmount;
   }
 
   function requestData() public payable returns (uint256 _value) {
@@ -20,8 +51,11 @@ contract Oracle {
   }
 
   function submitIntentionToStake() public payable {
-    require(msg.value == requiredStakeAmount, "You must the required amount to begin staking!");
+    require(msg.value == requiredStakeAmount, "You must send the required amount to begin staking!");
+    require(stakers[msg.sender] == StakerType.INACTIVE, "You must not be in a pending or active state.");
 
-    pendingStakers[msg.sender] = true;
+    totalStakeIntentionSupply += msg.value;
+
+    stakers[msg.sender] = StakerType.PENDING;
   }
 }
--- test/Oracle.js
+++ test/Oracle.js
@@ -5,6 +5,9 @@ contract("Oracle", async accounts => {
   // From initial_migration file
   const initialRequiredStakeAmount = 10000;
   const initialRequestAmount = 20000;
+  const INACTIVE_enum = 0;
+  const PENDING_enum = 1;
+  const ACTIVE_enum = 2;
 
   describe("initial deployment should correctly set", () => {
     it("request amount", async () => {
@@ -31,8 +34,11 @@ contract("Oracle", async accounts => {
   });
 
   describe("beginning to stake", () => {
-    it("should add sender to the pending stakers", async () => {
-      const instance = await Oracle.deployed();
+    it("should set sender to PENDING", async () => {
+      const instance = await Oracle.new(
+        initialRequiredStakeAmount,
+        initialRequestAmount
+      );
 
       const stakingAccount = accounts[1];
 
@@ -41,9 +47,33 @@ contract("Oracle", async accounts => {
         from: stakingAccount
       });
 
-      const staking = await instance.pendingStakers(stakingAccount);
+      const staking = await instance.stakers(stakingAccount);
 
-      assert.equal(staking, true);
+      assert.equal(staking, PENDING_enum);
+    });
+
+    it("should increase totalStakeIntentionSupply by stake amount for each pending user", async () => {
+      const instance = await Oracle.new(
+        initialRequiredStakeAmount,
+        initialRequestAmount
+      );
+
+      const stakingAccount1 = accounts[1];
+      const stakingAccount2 = accounts[2];
+
+      await instance.submitIntentionToStake({
+        value: initialRequiredStakeAmount,
+        from: stakingAccount1
+      });
+
+      await instance.submitIntentionToStake({
+        value: initialRequiredStakeAmount,
+        from: stakingAccount2
+      });
+
+      const stakingSupply = await instance.totalStakeIntentionSupply();
+
+      assert.equal(stakingSupply.toNumber(), initialRequiredStakeAmount * 2);
     });
 
     describe("should throw error", () => {
@@ -56,7 +86,7 @@ contract("Oracle", async accounts => {
 
         await truffleAssert.reverts(
           requestWithTooFewEth,
-          "You must the required amount to begin staking!"
+          "You must send the required amount to begin staking!"
         );
       });
 
@@ -69,9 +99,122 @@ contract("Oracle", async accounts => {
 
         await truffleAssert.reverts(
           requestWithNoEth,
-          "You must the required amount to begin staking!"
+          "You must send the required amount to begin staking!"
         );
       });
+
+      it("when sender is already pending", async () => {
+        const instance = await Oracle.deployed();
+
+        await instance.submitIntentionToStake({
+          value: initialRequiredStakeAmount
+        });
+
+        const alreadyPendingStakerRequest = instance.submitIntentionToStake({
+          value: initialRequiredStakeAmount
+        });
+
+        await truffleAssert.reverts(
+          alreadyPendingStakerRequest,
+          "You must not be in a pending or active state."
+        );
+      });
+
+      it("when sender is active", async () => {
+        const instance = await Oracle.deployed();
+
+        await instance.activateStake();
+
+        const requestWithNoEth = instance.submitIntentionToStake({
+          value: initialRequiredStakeAmount
+        });
+
+        await truffleAssert.reverts(
+          requestWithNoEth,
+          "You must not be in a pending or active state."
+        );
+      });
+    });
+  });
+
+  describe("activating staking", () => {
+    let instance;
+    const stakingAccount = accounts[1];
+
+    beforeEach(async () => {
+      instance = await Oracle.new(
+        initialRequiredStakeAmount,
+        initialRequestAmount
+      );
+
+      await instance.submitIntentionToStake({
+        value: initialRequiredStakeAmount,
+        from: stakingAccount
+      });
+    });
+
+    describe("by the first staker should", () => {
+      beforeEach(async () => {
+        await instance.activateStake({
+          from: stakingAccount
+        });
+      });
+      it("set totalMintedSupply to user's stake amount", async () => {
+        const totalMintedSupply = await instance.totalMintedSupply();
+
+        assert.equal(totalMintedSupply, initialRequiredStakeAmount);
+      });
+
+      it("set balance of the user as their stake amount", async () => {
+        const balanceOf = await instance.balanceOf(stakingAccount);
+
+        assert.equal(balanceOf, initialRequiredStakeAmount);
+      });
+    });
+
+    describe("by subsequent stakers should", () => {
+      const anotherStakingAccount = accounts[2];
+
+      beforeEach(async () => {
+        await instance.submitIntentionToStake({
+          value: initialRequiredStakeAmount,
+          from: anotherStakingAccount
+        });
+
+        await instance.activateStake({
+          from: stakingAccount
+        });
+
+        await instance.activateStake({
+          from: anotherStakingAccount
+        });
+      });
+
+      it("set totalMintedSupply as the total stake from users", async () => {
+        const totalMintedSupply = await instance.totalMintedSupply();
+
+        assert.equal(
+          totalMintedSupply.toNumber(),
+          initialRequiredStakeAmount * 2
+        );
+      });
+
+      it("set balance of the user as their stake amount", async () => {
+        const balanceOf = await instance.balanceOf(stakingAccount);
+
+        assert.equal(balanceOf, initialRequiredStakeAmount);
+      });
+    });
+
+    it("should error for non-pending user", async () => {
+      const requestWithNonPendingUser = instance.activateStake.call({
+        from: accounts[0]
+      });
+
+      await truffleAssert.reverts(
+        requestWithNonPendingUser,
+        "Must intially be a pending staker to activate..."
+      );
     });
   });
 
